f77955c53469cef2710a32fcba69124ef5922eb8
Huong <huongnguyen.nth1709@gmail.com>
2023-02-14 13:26:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// document.getElementById('count-el').innerText = 5;
+
+let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+}
+
+function save() {
+  let countStr = ' ' + count;
+  saveEl.innerText += countStr;
+  countEl.innerText = 0;
+  count = 0;
+}
